1e771ef7b8305308a387eb88295b42a54572a425
LiWaiYip <1155115790@link.cuhk.edu.hk>
2021-04-24 13:32:46 +0800

    passed hidden using Booth's algorithm

--- chapter5/hidden.java
+++ chapter5/hidden.java
@@ -8,23 +8,6 @@ import java.io.*;
 import java.util.*;
 
 public class hidden {
-    
-    // static void solve()
-    // {
-    //     int n = ni();
-    //     char[] carr = new char[n];
-    //     for (int i = 0; i < n; i++) {
-    //         carr[i] = nc();
-    //     }
-    //     String s = new String(carr);
-    //     s += s.substring(0, s.length() - 1);
-    //     int[] suf = SuffixArray.get(s);
-    //     int ans = suf[0];
-    //     out.printf("%d\n", ans);
-    // }
-
-    static int p = 29;
-    static int[] arr;
     static void solve()
     {
         int n = ni();
@@ -33,201 +16,36 @@ public class hidden {
             carr[i] = nc();
         }
         String s = new String(carr);
-        s = s + s.substring(0, s.length() - 1);
-        int[] suf = SuffixArray.get(s);
-        int start = -1;
-        for (int i = 0; i < suf.length; i++) {
-            if ((suf[i] + n - 1) < suf.length) {
-                start = suf[i];
-                break;
-            }
-        }
-        arr = new int[n*2-1];
-        for (int i = 0; i < arr.length; i++)
-            arr[i] = s.charAt(i) - 'a';
-        long want = hash(start, start+n-1);
-        long pow = 1;
-        for (int l = 1; l < n; l++)
-            pow *= p;
-        long h = hash(0, n-1);
-        boolean found = false;
-        int ans = start;
-        if (h == want) {
-            found = true;
-            ans = 0;
-        }
-        for (int i = 1; i <= arr.length - n && !found; i++) {
-            h = nextHash(pow, h, arr[i-1], arr[i+n-1]);
-            if (h == want) {
-                found = true;
-                ans = i;
-            }
-        }
+        int ans = leastRotation(s);
         out.printf("%d\n", ans);
     }
-
-
-
-// static boolean ok(int len)
-//     {
-//         Map<Long, Integer> mp = new HashMap<>();
-//         int n = arr.length;
-//         long h=hash(0, len-1);
-//         long pow = 1;
-//         mp.put(h, 0);
-//         for(int l=1;l<len;l++) pow*=p;
-//         for(int i=1;i<=n-len;i++){
-//             h=nextHash(pow,h,arr[i-1],arr[i+len-1]);
-//             if (mp.containsKey(h)) {
-//                 int start = mp.get(h);
-//                 if (start + len < i) return true;
-//             }
-//             else {
-//                 mp.put(h, i);
-//             }
-//         }
-//         return false;
-//     }
-
-static long nextHash(long pow,long hash,int left,int right){
-        return (hash - left*pow)*p + right;
-    }  
-
-    static long hash(int start, int end) {
-        long hash = 0;
-        long mul=1;
-        // for (int i = 0; i < length; i++) {
-        //     hash += arr[start+i]*mul;
-        //     mul*=p;
-        // }
-        for (int i = end; i >= start; i--) {
-            hash += arr[i]*mul;
-            mul*=p;
-        }
-        return hash;
-    }   
-
-
-
-static class SuffixArray
-{
-    // Class to store information of a suffix
-    public static class Suffix implements Comparable<Suffix>
+    static int leastRotation(String s)
     {
-        int index;
-        int rank;
-        int next;
- 
-        public Suffix(int ind, int r, int nr)
-        {
-            index = ind;
-            rank = r;
-            next = nr;
+        s = s + s;
+        int[] f = new int[s.length()];
+        Arrays.fill(f, -1);
+        int k = 0;
+        for (int j = 1; j < s.length(); j++) {
+            char sj = s.charAt(j);
+            int i = f[j - k - 1];
+            while (i != -1 && sj != s.charAt(k + i + 1)) {
+                if (sj < s.charAt(k + i + 1)) {
+                    k = j - i - 1;
+                }
+                i = f[i];
+            }
+            if (sj != s.charAt(k + i + 1)) {
+                if (sj < s.charAt(k)) {
+                    k = j;
+                }
+                f[j - k] = -1;
+            }
+            else {
+                f[j - k] = i + 1;
+            }
         }
-         
-        // A comparison function used by sort()
-        // to compare two suffixes.
-        // Compares two pairs, returns 1
-        // if first pair is smaller
-        public int compareTo(Suffix s)
-        {
-            if (rank != s.rank) return Integer.compare(rank, s.rank);
-            return Integer.compare(next, s.next);
-        }
+        return k;
     }
-     
-    // This is the main function that takes a string 'txt'
-    // of size n as an argument, builds and return the
-    // suffix array for the given string
-    public static int[] get(String s)
-    {
-        int n = s.length();
-        Suffix[] su = new Suffix[n];
-         
-        // Store suffixes and their indexes in
-        // an array of classes. The class is needed
-        // to sort the suffixes alphabatically and
-        // maintain their old indexes while sorting
-        for (int i = 0; i < n; i++)
-        {
-            su[i] = new Suffix(i, s.charAt(i) - '$', 0);
-        }
-        for (int i = 0; i < n; i++)
-            su[i].next = (i + 1 < n ? su[i + 1].rank : -1);
- 
-        // Sort the suffixes using the comparison function
-        // defined above.
-        Arrays.sort(su);
- 
-        // At this point, all suffixes are sorted
-        // according to first 2 characters.
-        // Let us sort suffixes according to first 4
-        // characters, then first 8 and so on
-        int[] ind = new int[n];
-         
-        // This array is needed to get the index in suffixes[]
-        // from original index. This mapping is needed to get
-        // next suffix.
-        for (int length = 4; length < 2 * n; length <<= 1)
-        {
-             
-            // Assigning rank and index values to first suffix
-            int rank = 0, prev = su[0].rank;
-            su[0].rank = rank;
-            ind[su[0].index] = 0;
-            for (int i = 1; i < n; i++)
-            {
-                // If first rank and next ranks are same as
-                // that of previous suffix in array,
-                // assign the same new rank to this suffix
-                if (su[i].rank == prev &&
-                    su[i].next == su[i - 1].next)
-                {
-                    prev = su[i].rank;
-                    su[i].rank = rank;
-                }
-                else
-                {
-                    // Otherwise increment rank and assign
-                    prev = su[i].rank;
-                    su[i].rank = ++rank;
-                }
-                ind[su[i].index] = i;
-            }
-             
-            // Assign next rank to every suffix
-            for (int i = 0; i < n; i++)
-            {
-                int nextP = su[i].index + length / 2;
-                su[i].next = nextP < n ?
-                   su[ind[nextP]].rank : -1;
-            }
-             
-            // Sort the suffixes according
-            // to first k characters
-            Arrays.sort(su);
-        }
-        // System.out.println();
-        // for (int i = 0; i < n; i++)
-        //     System.out.println(su[i].index + " " + su[i].rank);
-        // System.out.println();
-        // Store indexes of all sorted
-        // suffixes in the suffix array
-        int[] suf = new int[n];
-        for (int i = 0; i < n; i++)
-            suf[i] = su[i].index;
- 
-        // Return the suffix array
-        return suf;
-    }   
-} // This code is contributed by AmanKumarSingh
-
-
-
-
-
-
-
 
     /*I/O Template*/
     static InputStream is;
@@ -253,6 +71,7 @@ static class SuffixArray
         out.flush();
         long G = System.currentTimeMillis();
         tr(G-S+"ms");
+        System.out.println(G-S+"ms");
     }
     
     private static boolean eof()
