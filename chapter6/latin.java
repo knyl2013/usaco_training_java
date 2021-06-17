d08dc084e000b6c588548f3ec3a7e839fdb88808
Wai Yip <60865404+knyl2013@users.noreply.github.com>
2021-06-17 12:18:30 +0800

    Update latin (5/6)

--- chapter6/latin.java
+++ chapter6/latin.java
@@ -8,139 +8,350 @@ import java.io.*;
 import java.util.*;
 
 public class latin {
-	static int n;
-	static int[] facts = new int[8];
-	static int[][] perms;
-	static int[][] board;
-	
-	static void swap(int[] arr, int i, int j)
-	{
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
-	}
-	
-	static void reverse(int[] arr, int start, int end)
-	{
-		while (start < end) {
-			swap(arr, start++, end--);
+    static int n;
+    static int[][] board;
+    static boolean[][] rowappear, colappear;
+    static int[] rorders, corders;
+    static int ans = 0;
+    static int limit;
+    static int nax = (int) 1;
+    static int p = (int) 11;
+    static long[] memo = new long[nax];
+	static String[] cmps = new String[nax];
+    static int[] ppows = new int[100];
+    static int[][] perms;
+    static int[] facts = new int[8];
+    static Map<String, Long> mp = new HashMap<>();
+    static long[][] seen;
+    static void swap(int[] arr, int i, int j)
+    {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    
+    static void reverse(int[] arr, int start, int end)
+    {
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+    static int add(int a, int b)
+    {
+        int c = a + b;
+        if (c >= nax) {
+            c -= nax;
+        }
+        return c;
+    }
+    static int mul(int a, int b)
+    {
+        long c = (long) a * b;
+        return (int)(c % nax);
+    }
+    static void calcpows()
+    {
+        ppows[0] = 1;
+        for (int i = 1; i < ppows.length; i++) {
+            ppows[i] = mul(ppows[i-1], i);
+        }
+    }
+    static int hash(String s)
+    {
+        int ans = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            ans = add(ans, mul(ppows[i], s.charAt(i)));
+        }
+        return ans;
+    }
+	static int hash2()
+    {
+        List<Integer> lst = new ArrayList<>();
+		int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int cur = 0;
+            for (int j = 0; j < n; j++) {
+                if (colappear[i][j])
+                    cur = cur | (1 << j);
+            }
+            lst.add(cur);
+        }
+        Collections.sort(lst, (a, b) -> {
+            // int bita = Integer.bitCount(a), bitb = Integer.bitCount(b);
+            // if (bita != bitb) return bita - bitb;
+            return a - b;
+        });
+		int pidx = 0;
+		for (int x : lst) {
+			while (x>0) {
+				ans = add(ans, mul(ppows[pidx++], x%10));
+				x/=10;
+			}
+			ans = add(ans, mul(ppows[pidx++], 10));
+			// ans = add(ans, mul(ppows[pidx++], x));
 		}
-	}
-	
-	static void permutate()
-	{
-		perms = new int[facts[n]][];
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++)
-			arr[i] = i;
 		
-		perms[0] = arr.clone();
-		for (int i = 1; i < perms.length; i++) {
-			int maxsofar = arr[n-1];
-			int swapidx = -1;
-			for (int j = n-2; j >= 0; j--) {
-				if (arr[j] < maxsofar) {
-					swapidx = j;
-					break;
-				}
-				else {
-					maxsofar = arr[j];
-				}
-			}
-			int minidx = -1;
-			
-			for (int j = swapidx+1; j < n; j++) {
-				if (arr[j] > arr[swapidx]) {
-					if (minidx == -1 || arr[minidx] > arr[j])
-						minidx = j;
-				}
-			}
-			
-			swap(arr, minidx, swapidx);
-			reverse(arr, swapidx+1, n-1);
-			
-			perms[i] = arr.clone();
-		}
-	}
-	static boolean valid(int[][] board)
+		return ans;
+    }
+    static List<Integer> encode3(int r)
+    {
+        List<Integer> lst = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int cur = 0;
+            for (int j = 0; j < n; j++) {
+                if (colappear[i][j])
+                    cur = cur | (1 << j);
+            }
+            lst.add(cur);
+        }
+        Collections.sort(lst, (a, b) -> {
+            int bita = Integer.bitCount(a), bitb = Integer.bitCount(b);
+            if (bita != bitb) return bita - bitb;
+            return a - b;
+        });
+        int cur = 0;
+        for (int j = 0; j < n; j++) {
+            if (rowappear[r][j]) {
+                cur = cur | (1 << j);
+            }
+        }
+        lst.add(cur);
+        return lst;
+    }
+    static String encode2(int r)
+    {
+        List<Integer> lst = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int cur = 0;
+            for (int j = 0; j < n; j++) {
+                if (colappear[i][j])
+                    cur = cur | (1 << j);
+            }
+            lst.add(cur);
+        }
+        Collections.sort(lst, (a, b) -> {
+            // int bita = Integer.bitCount(a), bitb = Integer.bitCount(b);
+            // if (bita != bitb) return bita - bitb;
+            return a - b;
+        });
+        // int cur = 0;
+        // for (int j = 0; j < n; j++) {
+            // if (rowappear[r][j]) {
+                // cur = cur | (1 << j);
+            // }
+        // }
+        // lst.add(cur);
+        StringBuilder sb = new StringBuilder();
+        for (int x : lst) {
+            sb.append(x).append('-');
+        }
+        return sb.toString();
+    }
+    static void permutate()
+    {
+        perms = new int[facts[n]][];
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = i;
+        
+        perms[0] = arr.clone();
+        for (int i = 1; i < perms.length; i++) {
+            int maxsofar = arr[n-1];
+            int swapidx = -1;
+            for (int j = n-2; j >= 0; j--) {
+                if (arr[j] < maxsofar) {
+                    swapidx = j;
+                    break;
+                }
+                else {
+                    maxsofar = arr[j];
+                }
+            }
+            int minidx = -1;
+            
+            for (int j = swapidx+1; j < n; j++) {
+                if (arr[j] > arr[swapidx]) {
+                    if (minidx == -1 || arr[minidx] > arr[j])
+                        minidx = j;
+                }
+            }
+            
+            swap(arr, minidx, swapidx);
+            reverse(arr, swapidx+1, n-1);
+            
+            perms[i] = arr.clone();
+        }
+    }
+	static int f()
 	{
-		boolean[][] rowappear = new boolean[n][n];
-		boolean[][] colappear = new boolean[n][n];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				int val = board[i][j];
-				if (rowappear[i][val] || colappear[j][val])
-					return false;
-				rowappear[i][val] = true;
-				colappear[j][val] = true;
-			}
-		}
-		return true;
+		// int ans = 0;
+		// List<Integer> lst = new ArrayList<>();
+		// Set<Integer> set = new HashSet<>();
+		// for (int i = 0; i < n; i++) {
+		// 	int cur = 0;
+		// 	for (int j = 0; j < n; j++) {
+		// 		// if (i==j) continue;
+		// 		if (colappear[i][j]) {
+		// 			cur = cur | (1 << (j));
+		// 		}
+		// 		// if (!colappear[i][j]&& !colappear[j][i]) {
+		// 			// ans++;
+		// 		// }
+		// 	}
+		// 	lst.add(cur);
+		// 	set.add(cur);
+		// 	ans += cur;
+		// }
+		// System.out.println(lst);
+		// return set.size();
+		
+		// return ans;
+
+        int cnt = 0;
+        for (int[] perm : perms) {
+            int i;
+            for (i = 0; i < n; i++) {
+                int val = perm[i];
+                if (colappear[i][val]) break;
+            }
+            if (i == n) cnt++;
+        }
+        return cnt;
 	}
-	static void greedy(int[][] board)
-	{
-		boolean[][] rowappear = new boolean[n][n];
-		boolean[][] colappear = new boolean[n][n];
-		for (int i = 0; i < n; i++) {
-			colappear[i][i] = true;
-		}
-		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				board[i][j] = (i+j)%n;
-				// for (int d = 0; d < n; d++) {
-					// if (rowappear[i][d] || colappear[j][d])
-						// continue;
-					// rowappear[i][d] = true;
-					// colappear[j][d] = true;
-					// board[i][j] = d;
-					// break;
+    static String encode(int r)
+    {
+        char[] ans = new char[n*n+n];
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[idx++] = colappear[i][j] ? '1' : '0';
+            }
+        }
+        for (int i = 0; i < n; i++)
+            ans[idx++] = rowappear[r][i] ? '1' : '0';
+        return new String(ans);
+    }
+    static void dfs(int level)
+    {
+        if (level >= limit) {
+            ans++;
+            return;
+        }
+        int r = rorders[level], c = corders[level];
+        for (int i = 0; i < n; i++) {
+            if (rowappear[r][i] || colappear[c][i]) continue;
+            rowappear[r][i] = true;
+            colappear[c][i] = true;
+            dfs(level + 1);
+            rowappear[r][i] = false;
+            colappear[c][i] = false;
+        }
+    }
+    static long dfs(int r, int c)
+    {
+        if (r == n) {
+			// for (int[] row : board) {
+				// System.out.println(Arrays.toString(row));
+			// }
+			// System.out.println("==============");
+            return 1;
+        }
+        // int h = hash(encode2(r));
+		// int h = hash2();
+		String str = encode2(r);
+		// if (memo[h] != -1 && !cmps[h].equals(str)) {
+			// System.out.println(h + " " + cmps[h] + " " + str);
+		// }
+        // if (memo[h] != -1) return memo[h];
+		// cmps[h] = str;
+        
+        if (mp.containsKey(str)) {
+            // System.out.println("prune");
+            return mp.get(str);
+        }
+        // int fv = -1;
+        // if (c == 0) {
+            // fv = f();
+            // if (seen[r][fv] != -1) {
+                // return seen[r][fv];
+            // }
+        // }
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (rowappear[r][i] || colappear[c][i]) continue;
+            rowappear[r][i] = true;
+            colappear[c][i] = true;
+			board[r][c] = i;
+            int nextr = c == n-1 ? r + 1 : r;
+            int nextc = c == n-1 ? 1 : c + 1;
+            ans += dfs(nextr, nextc);
+            rowappear[r][i] = false;
+            colappear[c][i] = false;
+        }
+        mp.put(str, ans);
+        // if (c == 0) {
+            // seen[r][fv] = ans;
+        // }
+		// if (c == 0 && f() == 16) {
+			// System.out.println(r + " " + ans + " " + f());
+			// for (boolean[] row : rowappear) {
+				// for (boolean b : row) {
+					// System.out.print(b ? 1 : 0);
 				// }
-				// if (board[i][j] == -1) throw new RuntimeException(i + ", " + j);
-			}
-		}
-	}
-	static boolean[][] cols = new boolean[8][8];
-	static long f(int r)
-	{
-		if (r == n) return 1;
-		for (int i = 0; i < n; i++) {
-			int val = board[r-1][i];
-			cols[i][val] = true;
-		}
-		long cnt = 0;
-		for (int[] perm : perms) {
-			int i;
-			for (i = 0; i < n; i++) {
-				int val = perm[i];
-				if (cols[i][val]) break;
-			}
-			if (i == n) cnt++;
-		}
-		return cnt * f(r+1);
-	}
+				// System.out.println();
+			// }
+			// System.out.println();
+			// for (boolean[] row : colappear) {
+				// for (boolean b : row) {
+					// System.out.print(b ? 1 : 0);
+				// }
+				// System.out.println();
+			// }
+		// }
+        // memo[h] = ans;
+        return ans;
+    }
     static void solve()
     {
         n = ni();
-		facts[0] = 1;
-		for (int i = 1; i < facts.length; i++)
-			facts[i] = facts[i-1] * i;
-		permutate();
-		board = new int[n][n];
-		for (int[] row : board)
+        board = new int[n][n];
+        rowappear = new boolean[n][n];
+        colappear = new boolean[n][n];
+        limit = n*n-n;
+        rorders = new int[limit];
+        corders = new int[limit];
+        facts[0] = 1;
+        for (int i = 1; i < facts.length; i++)
+            facts[i] = facts[i-1] * i;
+        seen = new long[n][facts[facts.length-1]+1];
+		for (long[] row : seen)
 			Arrays.fill(row, -1);
-		for (int i = 0; i < n; i++)
-			board[0][i] = i;
-		greedy(board);
-		long ans = f(1);
-		out.printf("%d\n", ans);
-		// for (int[] row : board) {
-			// System.out.println(Arrays.toString(row));
-		// }
-		// System.out.println("valid: " + valid(board));
-		
-		// for (int[] p : perms) {
-			// System.out.println(Arrays.toString(p));
-		// }
+        calcpows();
+        permutate();
+        Arrays.fill(memo, -1);
+        int r = 1, c = 0;
+        for (int i = 0; i < limit; i++) {
+            rorders[i] = r;
+            corders[i] = c;
+            int nextr = r == n-1 ? 1 : r+1;
+            int nextc = r == n-1 ? c+1 : c;
+            r = nextr;
+            c = nextc;
+        }
+        for (int i = 0; i < n; i++) {
+            board[0][i] = i;
+            rowappear[0][i] = true;
+            colappear[i][i] = true;
+			
+			board[i][0] = i;
+			rowappear[i][i] = true;
+			colappear[0][i] = true;
+        }
+        // dfs(0);
+        long ans = dfs(1, 1) * facts[n-1];
+		// System.out.println(mp.size());
+        out.printf("%d\n", ans);
     }
 
 
@@ -151,9 +362,9 @@ public class latin {
     static InputStream is;
     static PrintWriter out;
     static String INPUT = "";
-    static String taskName = null;
-    // static String taskName = "latin";
-    static boolean logTime = true;
+    // static String taskName = null;
+    static String taskName = "latin";
+    static boolean logTime = !true;
     
     public static void main(String[] args) throws Exception
     {
