49abe6edada9bedd76e3ca071357001928cd549a
Wai Yip <60865404+knyl2013@users.noreply.github.com>
2021-07-07 17:11:21 +0800

    Update betsy.java

--- chapter6/betsy.java
+++ chapter6/betsy.java
@@ -20,8 +20,10 @@ public class betsy {
     static int fcnt;
     static long callCnt;
     static int want;
+	static int oneCnt = 0;
     static boolean ffSuccess;
 	static int[][][] mirror;
+	static int[][] chessboard;
     static int[][] bottomRightBadSample = new int[][] {
         new int[]{-1, 1, 0},
         new int[]{1, 0, 0},
@@ -258,10 +260,14 @@ public class betsy {
 				for (int a = 0; a < n; a++)
 					for (int b = 0; b < n; b++)
 						fillTable[a][b] = false;
+				fcnt = 0;
 				fillTable[i][n-1] = true;
 				floodFill(i, n-2);
 				want = -1;
 				boolean rcAtIsland = fillTable[r][c];
+				if (!rcAtIsland) return true;
+				int sameColor = chessboard[r][c] == chessboard[i][n-1] ? 1 : 0;
+				return fcnt % 2 == sameColor;
                 // for (int a = 0; a < n; a++) {
                     // for (int b = 0; b < n; b++) {
                         // if (fillTable[a][b]) {
@@ -270,7 +276,7 @@ public class betsy {
                         // }
                     // }
                 // }
-				return !rcAtIsland;
+				// return !rcAtIsland;
 			}
 		}
 		return false;
@@ -388,15 +394,16 @@ public class betsy {
         visitedCnt--;
         // curDp.put(bit, ans);
         visited[r][c] = false;
-		// if (ans != 0) {
-			// System.out.println("r: " + r + ", c: " + c);
-            // displayBoard();
-		// }
-        mp.put(key, ans);
-		if (ans == 86) {
+		if (ans == 1) {
+			oneCnt++;
 			// System.out.println("r: " + r + ", c: " + c);
             // displayBoard();
 		}
+        mp.put(key, ans);
+		// if (ans == 86) {
+			// System.out.println("r: " + r + ", c: " + c);
+            // displayBoard();
+		// }
 		// mp.put(mkey, ans);
 		// System.out.println("bit: " + Long.toBinaryString(bit) + ", ans: " + ans);
         if (ans == 0) {
@@ -463,14 +470,25 @@ public class betsy {
 	static void solve()
     {
 		int inp = ni();
+		chessboard = new int[inp][inp];
+		for (int i = 0; i < inp; i++) {
+			int curColor = i % 2 == 0 ? 0 : 1;
+			for (int j = 0; j < inp; j++) {
+				chessboard[i][j] = curColor;
+				curColor ^= 1;
+			}
+		}
+		// for (int[] row : chessboard) {
+			// System.out.println(Arrays.toString(row));
+		// }
 		if (inp == 7) {
-			five = helper(inp-2);
-			six = helper(inp-1);
+			// five = helper(inp-2);
+			// six = helper(inp-1);
 		}
         int ans = helper(inp);
         out.printf("%d\n", ans);
         if (logTime)
-            System.out.println("callCnt: " + callCnt);
+            System.out.println("callCnt: " + callCnt + ", oneCnt: " + oneCnt);
 		
 		System.out.println("five: " + five + ", six: " + six);
     }
